a065f1b283817326d10c1c824efb691d2253ddf6
odrling <florianbadie@odrling.xyz>
2024-08-07 04:05:07 +0200

    fonts: backoff and skip already uploaded fonts

--- wankotools/fonts.py
+++ wankotools/fonts.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 import logging
 from pathlib import Path
-from typing import Annotated
+from typing import Annotated, TypedDict
 
+import backoff
+import httpcore
 import httpx
 import typer
 
 logger = logging.getLogger(__name__)
 
 
+upload_backoff = backoff.on_exception(
+    backoff.expo,
+    (httpcore.ReadTimeout, httpx.ReadTimeout),
+    max_time=150,
+)
+
+
+@upload_backoff
 def upload_font(base_url: str, token: str, file: Path) -> None:
     headers = {"Authorization": f"Bearer {token}"}
     with file.open("rb") as f:
@@ -18,12 +28,33 @@ def upload_font(base_url: str, token: str, file: Path) -> None:
         resp.raise_for_status()
 
 
+class Fonts(TypedDict):
+    ID: int
+    Name: str
+
+
+class FontResponse(TypedDict):
+    Fonts: list[Fonts]
+
+
+def get_uploaded_fonts(base_url: str, token: str) -> list[Fonts]:
+    headers = {"Authorization": f"Bearer {token}"}
+    resp = httpx.get(f"{base_url}/api/font", headers=headers)
+    data: FontResponse = resp.json()
+    resp.raise_for_status()
+    return data["Fonts"]
+
+
 def main(
     files: list[Annotated[Path, typer.Argument(file_okay=True, dir_okay=False)]],
     base_url: Annotated[str, typer.Option(help="base url of your karaberus instance")],
     token: Annotated[str, typer.Option(help="your access token")],
 ) -> None:
+    fonts = get_uploaded_fonts(base_url, token)
+    font_names = {font["Name"] for font in fonts}
     for file in files:
+        if file.name in font_names:
+            continue
         upload_font(base_url, token, file)
 
 
